6632743f19a86865b824c8fa03b477064dad3d37
Max Maischein <corion@corion.net>
2016-04-24 20:48:09 +0200

    Add License sections etc.

--- lib/Archive/Merged.pm
+++ lib/Archive/Merged.pm
@@ -136,6 +136,36 @@ sub extract_file {
 
 1;
 
+=head1 REPOSITORY
+
+The public repository of this module is 
+L<http://github.com/Corion/archive-merged>.
+
+=head1 SUPPORT
+
+The public support forum of this module is
+L<https://perlmonks.org/>.
+
+=head1 BUG TRACKER
+
+Please report bugs in this module via the RT CPAN bug queue at
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=Archive-Merged>
+or via mail to L<archive-merged-Bugs@rt.cpan.org>.
+
+=head1 AUTHOR
+
+Max Maischein C<corion@cpan.org>
+
+=head1 COPYRIGHT (c)
+
+Copyright 2015-2016 by Max Maischein C<corion@cpan.org>.
+
+=head1 LICENSE
+
+This module is released under the same terms as Perl itself.
+
+=cut
+
 =head1 SEE ALSO
 
 L<Archive::Tar>
